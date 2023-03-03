302f7b6260b1d5579e0e86fc0179c9c4ac75ea1f
rica213 <25f5k252l@mozmail.com>
2023-03-03 17:02:58 +0300

    Add routes and navbar to the application

---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+const Books = () => (
+  <>
+    <h1>List of books</h1>
+    <form>
+      <input type="text" placeholder="new book" />
+      <input type="text" placeholder="author" />
+      <button type="button">Add book</button>
+    </form>
+  </>
+);
+
+export default Books;
