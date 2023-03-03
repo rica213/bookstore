a54660a6c961906a04afc17257727606bd6ac118
rica213 <25f5k252l@mozmail.com>
2023-03-03 18:56:49 +0300

    Create Form component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,10 @@
 import Book from './Book';
+import Form from './Form';
 
 const Books = () => (
   <>
     <Book title="Book one" author="Author one" />
-    <form>
-      <input type="text" placeholder="new book" />
-      <input type="text" placeholder="author" />
-      <button type="button">Add book</button>
-    </form>
+    <Form />
   </>
 );
 
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+const Form = () => (
+  <form>
+    <input type="text" placeholder="title" />
+    <input type="text" placeholder="author" />
+    <button type="button">Add book</button>
+  </form>
+);
+
+export default Form;
