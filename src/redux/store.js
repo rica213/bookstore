403603a6e36693f1f1ab0c3adf140712993a9950
rica213 <25f5k252l@mozmail.com>
2023-03-08 17:14:54 +0300

    Add categories slice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-import { configureStore} from '@reduxjs/toolkit';
-import booksReducer from "./books/booksSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     books: booksReducer,
+    categorie: categoriesReducer,
   },
 });
+
+export default store;
