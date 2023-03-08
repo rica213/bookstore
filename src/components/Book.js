ddfb001f7c0b6d6826f8a84fdd450ca9b77320eb
rica213 <25f5k252l@mozmail.com>
2023-03-08 16:32:14 +0300

    Create remove action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,19 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { removeBook } from "../redux/books/booksSlice";
 
-const Book = ({ title, author }) => (
-  <>
-    <h3>{title}</h3>
-    <p>{author}</p>
-    <button type="button">Remove</button>
-  </>
-);
-
+const Book = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <button type="button" onClick={() => {dispatch(removeBook(id))}}>
+        Remove
+      </button>
+    </>
+  );
+};
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -10,14 +10,18 @@ const booksSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       const newBook = {
+        id: `item${state.books.length + 1}`,
         title: action.payload.title,
         author: action.payload.author
       }
       state.books.push(newBook);
     },
+    removeBook: (state, {payload}) => {
+      state.books.filter(book => book.id !== payload.id)
+    }
   }
 });
 
-export const { addBook } = booksSlice.actions;
+export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
