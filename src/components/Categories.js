9bb0df793fd110c6f97244cc8d2d6e695c0c7944
rica213 <25f5k252l@mozmail.com>
2023-03-11 17:12:04 +0300

    Add styling to the categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,21 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categoriesSlice';
+import styles from '../styles/Categories.module.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories);
   return (
-    <>
+    <article className={styles.container}>
       <button
         type="button"
         onClick={() => {
           dispatch(checkStatus());
         }}
+        className={styles.checkStatus}
       >
         Check status
       </button>
       <p>{categories}</p>
-    </>
+    </article>
   );
 };
 
